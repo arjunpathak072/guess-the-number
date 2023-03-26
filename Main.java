5cb09b3698efa8d436c9474015fa3bc7f3b3f6a2
= <=>
2023-03-27 00:59:04 +0530

    Added variable declarations and messages

--- Main.java
+++ Main.java
@@ -4,5 +4,13 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random ra = new Random();
+
+        int difficultyMode, tries = 5, choice = 1;
+
+        System.out.println("\nWelcome to the guessing game: \n");
+        System.out.println("1 => Easy\n2 => Hard\n3 => Harder\n4 => Hardest");
+
     }
 }
